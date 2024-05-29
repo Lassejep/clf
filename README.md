362dc1257f02efe7237229d45437484c62d81c03
lassejep <lasse.jepsen@hotmail.com>
2024-05-29 16:22:20 +0200

    formatting change and updated readme and help command

--- README.md
+++ README.md
@@ -3,15 +3,15 @@ Command Line Fuzzer
 
 Fuzz a command with a wordlist and check for a condition in the output.
 
-| options |
 | Command | Description |
 | ------- | ----------- |
 | -h, --help | Show help message and exit |
-| -e EXECUTE, --execute EXECUTE | The command to execute with FUZZ as the argument placeholder |
+| -e COMMAND, --execute COMMAND | The command to execute with FUZZ as the argument placeholder |
 | -w WORDLIST, --wordlist WORDLIST | The wordlist to use as input to the command |
 | -c CONDITION, --condition CONDITION | The condition to check for in the output |
 | -i, --ignore-error | Ignore errors and continue fuzzing |
 | -s, --sanitize | Sanitize input to prevent ' " ` and \ characters from breaking the command |
+| -t THREADS, --threads THREADS | The number of threads to use for fuzzing |
 
 ## Examples
 ```bash
--- clf.py
+++ clf.py
@@ -3,9 +3,11 @@ import codecs
 import argparse
 import asyncio
 
+
 def close():
     print('[-]Exiting...')
     exit(0)
+
 
 def sanitize_input(input):
     input = input.replace('\\', '\\\\')
@@ -14,6 +16,7 @@ def sanitize_input(input):
     input = input.replace('`', '\\`')
     return input
 
+
 def build_command(command, wordlist):
     with codecs.open(wordlist, 'r', encoding='utf-8', errors='ignore') as f:
         for word in f:
@@ -21,14 +24,19 @@ def build_command(command, wordlist):
                 word = sanitize_input(word)
             yield command.replace('FUZZ', f'"{word.strip()}"')
 
+
 def count_lines(file):
     with codecs.open(file, 'r', encoding='utf-8', errors='ignore') as f:
         return sum(1 for _ in f)
 
+
 async def run_command(command):
-    process = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    process = await asyncio.create_subprocess_shell(
+        command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
     stdout, stderr = await process.communicate()
     return stdout, stderr
+
 
 async def queue_worker(queue):
     while True:
@@ -47,13 +55,15 @@ async def queue_worker(queue):
                 print(f'[-]Error: {stderr.decode()}')
         queue.task_done()
 
+
 async def run():
-    print(f'Loading wordlist...')
+    print('Loading wordlist...')
     total_lines = count_lines(args.wordlist)
     completed_lines = 0
     tasks = []
     command_generator = build_command(args.execute, args.wordlist)
     queue = asyncio.Queue(maxsize=args.threads)
+    task = None
     for command in command_generator:
         completed_lines += 1
         if queue.full():
@@ -66,13 +76,33 @@ async def run():
         print(f'[{completed_lines}/{total_lines}]', end='\r')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Fuzz a command with a list of arguments')
-    parser.add_argument('-e', '--execute', type=str, help='The command to execute with FUZZ as the argument placeholder')
-    parser.add_argument('-w', '--wordlist', type=str, help='The wordlist to use as input to the command')
-    parser.add_argument('-c', '--condition', type=str, help='The condition to check for in the output')
-    parser.add_argument('-i', '--ignore-error', action='store_true', help='Ignore errors and continue fuzzing')
-    parser.add_argument('-s', '--sanitize', action='store_true', help='Sanitize input to prevent \' \" ` and \\ characters from breaking the command')
-    parser.add_argument('-t', '--threads', type=int, default=4, help='Number of threads to use for fuzzing')
+    parser = argparse.ArgumentParser(
+        description='Fuzz a command with a list of arguments')
+    parser.add_argument(
+        '-e', '--execute', type=str, metavar='COMMAND',
+        help='The command to execute with FUZZ as the argument placeholder'
+    )
+    parser.add_argument(
+        '-w', '--wordlist', type=str, metavar='WORDLIST',
+        help='The wordlist to use as input to the command'
+    )
+    parser.add_argument(
+        '-c', '--condition', type=str, metavar='CONDITION',
+        help='The condition to check for in the output'
+    )
+    parser.add_argument(
+        '-i', '--ignore-error', action='store_true',
+        help='Ignore errors and continue fuzzing'
+    )
+    parser.add_argument(
+        '-s', '--sanitize', action='store_true',
+        help='''Sanitize input to prevent \' \" ` and \\
+        characters from breaking the command'''
+    )
+    parser.add_argument(
+        '-t', '--threads', type=int, default=4,
+        help='Number of threads to use for fuzzing'
+    )
     args = parser.parse_args()
 
     if not args.execute or not args.wordlist:
